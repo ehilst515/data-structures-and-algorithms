3498b1a271e215f0db970405a0f2e171fae28288
Enrique Hilst <enrique.hilst@gmail.com>
2020-07-21 17:58:45 -0500

    stretch goals complete

--- code-challenges/value-vs-reference/challenges-02.test.js
+++ code-challenges/value-vs-reference/challenges-02.test.js
@@ -80,7 +80,9 @@ console.log(people[1].isAuthor) prints true
 ------------------------------------------------------------------------------------------------ */
 
 const setStatusAsAuthor = (people) => {
-  
+  people.forEach(function(person){
+    person.isAuthor = true;
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -99,8 +101,9 @@ console.log(a) prints [1, 2, 3, 4]
 ------------------------------------------------------------------------------------------------ */
 
 const append = (arr1, arr2) => {
-  // Solution code here...
-
+  arr2.forEach(function(val){
+    arr1.push(val);
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -159,7 +162,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should append the second array to the first', () => {
     const a = [1, 2, 3, 4];
     const b = [5, 6, 7, 8];
